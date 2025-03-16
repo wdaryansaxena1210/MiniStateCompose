4bba1e5641c8c0db71e4539a18b113762211f369
Aryan Saxena <aryansaxena1210@gmail.com>
2025-03-15 19:56:48 -0500

    second push

---
+++ app/src/main/java/com/example/ministate/presentation/EventCatagoryScreen.kt
@@ -0,0 +1,7 @@
+package com.example.ministate.presentation
+
+class EventCatagoryScreen(
+    onClick : (String) -> Unit
+) {
+
+}
--- app/src/main/java/com/example/ministate/presentation/MainActivity.kt
+++ app/src/main/java/com/example/ministate/presentation/MainActivity.kt
@@ -8,7 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.ministate.common.TestFunctions
 import com.example.ministate.data.remote.repository.EventRepositoryImpl
 import com.example.ministate.presentation.ui.theme.MiniStateTheme
@@ -30,7 +35,7 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding)
                             .fillMaxSize()
                     ) {
-
+                        Navigation(viewModel)
                     }
                 }
             }
@@ -45,6 +50,19 @@ class MainActivity : ComponentActivity() {
 //            testFunctions.testEventRepositoryGetEventDetailsList()
 //
 //        }
+    }
+
+    @Composable
+    private fun Navigation(viewModel: EventViewModel) {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "event_catagory_screen") {
+            composable("event_catagory_screen") {
+                EventCatagoryScreen(
+                    onClick = { println("navigate to $it")},
+
+                )
+            }
+        }
     }
 }
 
